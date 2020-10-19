27abb299fc0d5994298bdd15068e191aa4742826
jonas-santiago <jonas.s.santiago@gmail.com>
2020-10-18 23:45:00 -0300

    DepartmentDaoJDBC Implementation

--- src/model/Dao/DaoFactory.java
+++ src/model/Dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.Dao;
 
 import db.DB;
+import model.Dao.Impl.DepartmentDaoJDBC;
 import model.Dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
